98ec43b656abd7465a5c2c4c7342b223a3d9f9b2
aayush <hiaayush30@gmail.com>
2025-02-07 19:18:47 +0530

    completed simple chat room

--- Websockets Project-Chat App/Backend/src/index.ts
+++ Websockets Project-Chat App/Backend/src/index.ts
@@ -2,55 +2,87 @@ import { WebSocket, WebSocketServer } from 'ws'
 import crypto from 'crypto'
 const wss = new WebSocketServer({ port: 8000 });
 
-let userCount=0;
-interface roomObject{
-    code:string;
-    sockets:Array<WebSocket>
+let userCount = 0;
+interface roomObject {
+    room: string;
+    sockets: Array<WebSocket>
 }
-const getRoomCode = ()=> crypto.randomBytes(2).toString('hex');
-let currRoom:string=getRoomCode();
-let rooms:Array<roomObject>=[{  //creating the initial room
-    code:currRoom,
-    sockets:[]
-}];
-wss.on('connection',(ws) => {
-    if(userCount>=2){
-        currRoom = getRoomCode();
-        rooms.push({
-            code:currRoom,
-            sockets:[ws]
-        });
-        userCount=1;
-        ws.send('waiting for another user...');
-    }
-    else if (userCount == 0){                //1st ever user
-        const targetRoom = rooms.find(room=>room.code == currRoom);
-        targetRoom?.sockets.push(ws);
-        ws.send('waiting for another user...');
-        userCount++;
-    }
-    else{
-        const targetRoom = rooms.find(room=>room.code == currRoom);
-        targetRoom?.sockets.push(ws);
-        userCount++;
-        targetRoom?.sockets.forEach((socket)=>{
-          socket.send('new user joined!')
-        })
-    }
+const getRoomCode = () => crypto.randomBytes(2).toString('hex');
+let rooms: Array<roomObject> = [];
+
+wss.on('connection', (ws) => {
+    userCount++;
     console.log(userCount);
-    ws.on('message', msg => {
-        console.log(rooms);
-        rooms.forEach(room=>{
-            if(room.sockets.includes(ws)){
-                room.sockets.forEach(ws=>ws.send(msg))
+    ws.on('message', async msg => {
+        let message;
+        if (msg instanceof Blob) {
+            message = await msg.text();
+        } else {
+            message = msg.toString();
+        }
+        message = JSON.parse(message);
+        console.log(message);
+        if (message?.type == 'join') {
+            if (message.payload?.roomId) {  //if roomId present in payload
+                //find if such a room exists
+                if (!rooms.some(roomObj => roomObj.room == message.payload.roomId)) {
+                    ws.send(JSON.stringify({
+                        type:'join',
+                        payload: {
+                            message: 'no room found!'
+                        }
+                    }))
+                    ws.close();
+                } else {   //add to the desired room
+                    const roomObj = rooms.find(roomObj => roomObj.room == message.payload.roomId);
+                    roomObj?.sockets.push(ws);
+                    ws.send(JSON.stringify({
+                        type: 'join',
+                        payload: {
+                            message: 'connected to ' + message.payload.roomId,
+                            roomId: message.payload.roomId
+                        }
+                    }))
+                }
+                //create a new room
+            } else {
+                const roomId = getRoomCode();
+                rooms.push({
+                    room: roomId,
+                    sockets: [ws]
+                })
+                ws.send(JSON.stringify({
+                    type: 'join',
+                    payload: {
+                        message: 'you joined room ' + roomId,
+                        roomId
+                    }
+                }))
+            }
+        } else {               //if message.type =='chat'
+            rooms.forEach(roomObj => {
+                if (roomObj.sockets.includes(ws)) {
+                    roomObj.sockets.forEach(socket => {
+                        socket.send(JSON.stringify({
+                            type: 'chat',
+                            payload: {
+                                message:message.payload?.message
+                            }
+                        }));
+                    })
+                }
+            })
+        }
+    })
+
+    ws.on('close',()=>{
+        rooms.forEach(roomObj=>{
+            if(roomObj.sockets.includes(ws)){
+                roomObj.sockets=roomObj.sockets.filter(socket=>socket!=ws);
+            }
+            if(roomObj.sockets.length==0){  //remove the empty rooms
+                rooms=rooms.filter(room=>room.room == roomObj.room);
             }
         })
-    });
-    ws.on('close',()=>{
-        const closingRoom=rooms.find(room=>room.sockets.includes(ws));
-        closingRoom?.sockets.forEach(socket=>{   //remove the room as only 2 sockets present
-            socket.close()
-        })
-        rooms=rooms.filter(room=>room!=closingRoom); 
     })
 })
--- Websockets Project-Chat App/Frontend/src/App.tsx
+++ Websockets Project-Chat App/Frontend/src/App.tsx
@@ -1,27 +1,83 @@
-import React, { FormEvent, useEffect, useRef, useState } from 'react'
+import { FormEvent, useEffect, useRef, useState } from 'react'
 
 const App = () => {
   const [text, setText] = useState('');
-  const [socket, setSocket] = useState<WebSocket>();
+  const [socket, setSocket] = useState<WebSocket | null>();
   const [chat, setChat] = useState<Array<string>>([]);
-  useEffect(() => {
+  const [roomCode, setRoomCode] = useState('');
+  const [joinRoomCode, setJoinRoomCode] = useState('');
+
+  const handleNewRoom = () => {
     const ws = new WebSocket('ws://localhost:8000');
-    setSocket(ws);
-    ws.onmessage = async (e: MessageEvent) => {
-      if (e.data instanceof Blob) {
-        const text = await e.data.text(); // Convert Blob to text
-        setChat(chat => [...chat, text]);
+    ws.onopen = () => {
+      setSocket(ws);
+      ws.send(JSON.stringify({
+        type: 'join'
+      }));
+    }
+    ws.onmessage = (e: MessageEvent) => {
+      const message = JSON.parse(e.data);
+      console.log(message);
+      if (message.type == 'join') {
+        if (message.payload.roomId) {
+          setRoomCode(message.payload.roomId);
+        }
+        setChat(chat => [...chat, message.payload.message]);
       } else {
-        setChat(chat => [...chat, e.data]);
+        setChat(chat => [...chat, message.payload.message])
       }
     }
-    ws.onclose=()=>{
-           alert('chat closed!')
+    ws.onclose = () => {
+      alert('session closed!');
+      setChat(chat => [...chat, "session terminated"]);
+      setSocket(null);
     }
-  }, [])
+  }
+  const handleJoinRoom = () => {
+    if (joinRoomCode.trim().length != 4) {
+      return alert('Enter a valid code!');
+    }
+    const ws = new WebSocket('ws://localhost:8000');
+    ws.onopen = () => {
+      setSocket(ws);
+      ws.send(JSON.stringify({
+        type: 'join',
+        payload: {
+          roomId: joinRoomCode
+        }
+      }));
+      ws.onmessage = (e: MessageEvent) => {
+        const message = JSON.parse(e.data);
+        console.log(message);
+        if (message.type == 'join') {
+          if (message.payload.roomId) {
+            setRoomCode(message.payload.roomId);
+          }
+          setChat(chat => [...chat, message.payload.message]);
+        } else {
+          setChat(chat => [...chat, message.payload.message])
+        }
+      }
+      ws.onclose = () => {
+        alert('session closed!');
+        setChat(chat => [...chat, "session terminated"]);
+        setSocket(null);
+      }
+    }
+  }
+
+  const handleCloseSocket = () => {
+    socket?.close();
+    setSocket(null);
+  }
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    socket?.send(text);
+    socket?.send(JSON.stringify({
+      type: 'chat',
+      payload: {
+        message: text
+      }
+    }));
     setText('');
   }
 
@@ -32,27 +88,41 @@ const App = () => {
   return (
     <div className='h-screen flex flex-col justify-center bg-zinc-800 text-white'>
       <h1 className='text-center text-2xl pt-5'
-      >Chat App Prototype</h1>
-
-      <div className='w-[50vw] mx-auto'>
+      >Chat Rooms : Chat and Forget</h1>
+      <div className='flex justify-center items-center gap-3'>
+        <p className=''>Room Code:</p>
+        <input className='w-16 text-slate-200' disabled type='text' value={roomCode} />
+      </div>
+      <div className='w-[50vw] mx-auto max-sm:w-[80vw]'>
         <div className='pb-5 text-black relative justify-end rounded-md  items-center bg-red-200 h-[50vh] mt-5 overflow-y-auto'>
           <div className='h-full flex flex-col'>
-            {chat.map(msg => {
-              return <div className='bg-red-300 self-center p-1 rounded-lg m-1'
+            {chat.map((msg, index) => {
+              return <div key={index} className='bg-red-300 self-center p-1 rounded-lg m-1'
               >{msg}</div>
             })}
-            <div ref={anchorRef} className="h-1 bg-green-300"></div>
+            <div ref={anchorRef} className="h-1"></div>
           </div>
         </div>
         <form onSubmit={handleSubmit}
           className=' text-center mt-2'>
-          <input value={text} onChange={(e) => setText(e.target.value)}
-            className=' bg-slate-200 text-black p-1 rounded-md outline-none'
-            placeholder='Type a messaage' />
-          <button type='submit'
-            className='mx-2 bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600'
-          >Send</button>
+          {socket && <>
+            <input value={text} onChange={(e) => setText(e.target.value)}
+              className=' bg-slate-200 text-black p-1 rounded-md outline-none'
+              placeholder='Type a messaage' />
+            <button type='submit'
+              className='mx-2 bg-zinc-700 p-1 rounded-md cursor-pointer hover:bg-zinc-600'
+            >Send</button>
+            <button onClick={handleCloseSocket}
+              className='bg-red-600 max-sm:absolute max-sm:top-2 max-sm:left-2 text-slate-200 rounded-md p-1 cursor-pointer'>TERMINATE</button></>}
         </form>
+        {!socket && <div className='flex gap-5 items-center justify-center w-full'>
+          <button className='cursor-pointer p-1 bg-blue-600 rounded-md' onClick={handleNewRoom}>new chat</button>
+          <div className='border-2 rounded-md'>
+            <input value={joinRoomCode} onChange={e => setJoinRoomCode(e.target.value)}
+              type='text' placeholder='room code' className='p-1 outline-none' />
+            <button onClick={handleJoinRoom} className='bg-blue-600 p-1 cursor-pointer rounded-md'>JOIN</button>
+          </div>
+        </div>}
       </div>
     </div>
   )
